9ed6076a93c8233ff94a3c4697bbaa21680379e5
jsimplicio <juliarsimplicio@gmail.com>
2020-06-23 19:08:23 -0500

    Measurements app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const scaleNames = {
+  c: 'Cups',
+  f: 'Ounces'
+};
+
+function toCups(Ounces) {
+  return Ounces / 8;
 }
 
-export default App;
+function toOunces(Cups) {
+  return Cups * 8;
+}
+
+function tryConvert(value, convert) {
+  const input = parseFloat(value);
+  if (Number.isNaN(input)) {
+    return '';
+  }
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}
+
+class UnitInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.props.onChange(e.target.value);
+  }
+
+  render() {
+    const value = this.props.value;
+    const scale = this.props.scale;
+    return (
+      <div className="container">
+        <form>
+          <div className= "form-group">
+            <lable><h3>Enter measurement in {scaleNames[scale]} </h3></lable>
+            <input className="form-control text-center" id="focusedInputed" type="text" value={value}
+              onChange={this.handleChange} />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleCupsChange = this.handleCupsChange.bind(this);
+    this.handleOuncesChange = this.handleOuncesChange.bind(this);
+    this.state = {value: '', scale: 'c'};
+  }
+
+  handleCupsChange(value) {
+    this.setState({scale: 'c', value});
+  }
+
+  handleOuncesChange(value) {
+    this.setState({scale: 'f', value});
+  }
+
+  render() {
+    const scale = this.state.scale;
+    const value = this.state.value;
+    const Cups = scale === 'f' ? tryConvert(value, toCups) : value;
+    const Ounces = scale === 'c' ? tryConvert(value, toOunces) : value;
+
+    return (
+      <div className="text-center container-fluid">
+        <UnitInput
+          scale="c"
+          value={Cups}
+          onChange={this.handleCupsChange} />
+        <UnitInput
+          scale="f"
+          value={Ounces}
+          onChange={this.handleOuncesChange} />
+      </div>
+    );
+  }
+}
+
+export default Calculator;
